221f947c4ed94992cba27d5c3efd73bf3d699a9f
Azizjon <macbookairm2@MacBook-Air-MacBook.local>
2025-10-02 00:32:27 +0500

    added new domain

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ admin.initializeApp({
 
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://127.0.0.1:5173'], // Multiple frontend URLs
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://127.0.0.1:5173', 'https://myportfolio-uy9s.vercel.app/'], // Multiple frontend URLs
     credentials: true, // Allow cookies/credentials
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'authtoken'],
